a66b6d180085582c6ed7a9af4050dfa6a4ab1004
Adam Gąsowski <gander@users.noreply.github.com>
2025-11-16 19:26:25 +0100

    Move unit tests and update Vitest configuration

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
-import App from "../App.vue";
+import App from "@/App.vue";
 
 describe("App", () => {
 	it("mounts renders properly", () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default mergeConfig(
 			environment: "jsdom",
 			exclude: [...configDefaults.exclude, "e2e/**"],
 			root: fileURLToPath(new URL("./", import.meta.url)),
+			include: ["tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
 		},
 	}),
 );
